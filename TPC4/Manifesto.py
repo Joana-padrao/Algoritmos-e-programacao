8839e43597d9353ccc5ee40b39b28ba47d928bd1
Joana-padrao <92030804+Joana-padrao@users.noreply.github.com>
2021-10-29 21:22:51 +0100

    TPC4

---
+++ TPC4/Manifesto.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Manifesto
+
+# * __identificador:__ TPC3
+# * __título:__ frações
+# * __data de início:__ 2021-10-25
+# * __data de fim:__ 2021-10-29
+# * __supervisor:__ José Carlos Ramalho, url: https://www.di.uminho.pt/~jcr/thesup-v2.php
+# * __autor:__ Joana Padrão, nº a96101
+# * __resumo:__ Ao iniciar deparamo-nos com um menu com varias opções e cada uma delas levará a uma função diferente. É possível  criar uma fração (opção 1), indicando qual o numerador e qual o denominador; simplificar uma fração (opção 2); realizar somas, subtrações, multiplicações e divisões de frações (opções 3,4,5 e 6 respetivamente); criar uma lista com _n_ frações aleatórias (opção 7); calcular a soma de todas as frações da lista criada na opção 7 (opção 8); ou por último descobrir qual a maior fração da lista criada anteriormente na opção 7 (opção 9).
+
+# In[ ]:
+
+
+
+
